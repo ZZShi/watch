5246a0723d4f971c53a251d667720f4ac3efba4c
史卓洲 <zhuozhou.shi@longbridge.sg>
2022-05-28 17:18:44 +0800

    [add] - 创建目录结构

---
+++ backend/api/v1/endpoints/login.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+
+app = APIRouter(tags=["认证相关"])
+
+
+@app.post("/login", summary="登录")
+def user_login():
+    pass
+
+
+@app.put("/logout", summary="退出")
+def user_logout():
+    pass
+
+
+@app.post("/user", summary="新增用户")
+def user_create():
+    pass
